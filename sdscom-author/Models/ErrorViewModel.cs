8c1e06361245f2c30bb1f37bec4a6e6fe8e41785
hazcom coder <hazcom.coder@gmail.com>
2017-12-06 21:19:20 -0800

    Add project files.

---
+++ sdscom-author/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sdscom_author.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
